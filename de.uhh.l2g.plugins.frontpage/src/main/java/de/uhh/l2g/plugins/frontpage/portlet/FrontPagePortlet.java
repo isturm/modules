105e3b7071b1fdf7a3377fc56a0dbd922eb6f3ea
isturm <iavor.sturm@uni-hamburg.de>
2017-12-14 10:51:02 +0100

    service update.

--- de.uhh.l2g.plugins.frontpage/src/main/java/de/uhh/l2g/plugins/frontpage/portlet/FrontPagePortlet.java
+++ de.uhh.l2g.plugins.frontpage/src/main/java/de/uhh/l2g/plugins/frontpage/portlet/FrontPagePortlet.java
@@ -1,10 +1,23 @@
 package de.uhh.l2g.plugins.frontpage.portlet;
 
 import de.uhh.l2g.plugins.frontpage.constants.FrontPagePortletKeys;
+import de.uhh.l2g.plugins.model.Video;
+import de.uhh.l2g.plugins.service.VideoLocalServiceUtil;
 
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -14,7 +27,7 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=category.l2g",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=de.uhh.l2g.plugins.frontpage Portlet",
 		"javax.portlet.init-param.template-path=/",
@@ -26,4 +39,23 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class FrontPagePortlet extends MVCPortlet {
+	@Override
+	public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+		super.doView(renderRequest, renderResponse);
+		
+		List<User> ul = UserLocalServiceUtil.getNoGroups();
+		
+		try{
+			List<Video> all = VideoLocalServiceUtil.getAll();
+		}catch (Exception e){
+			int i =0;
+			i++;
+			//		
+		}
+	}
+	
+	public void testAction(ActionRequest actionRequest, ActionResponse response) {
+		//
+	}
+	
 }
--- de.uhh.l2g.plugins.frontpage/src/main/resources/META-INF/resources/view.jsp
+++ de.uhh.l2g.plugins.frontpage/src/main/resources/META-INF/resources/view.jsp
@@ -1,16 +1,3 @@
-<%@page import="de.uhh.l2g.plugins.service.VideoLocalServiceUtil"%>
-<%@page import="de.uhh.l2g.plugins.model.Video"%>
-
-<%@page import="java.util.ListIterator"%>
-<%@page import="java.util.List"%>
-
-<%
-	//get popular videos
-	//example -> top 10
-	List<Video> popular = VideoLocalServiceUtil.getPopular(12);
-	ListIterator<Video> pli = popular.listIterator();
-%>
-
 <%@ include file="/init.jsp" %>
 <p>
 	<b><liferay-ui:message key="de.uhh.l2g.plugins.frontpage.caption"/></b>
