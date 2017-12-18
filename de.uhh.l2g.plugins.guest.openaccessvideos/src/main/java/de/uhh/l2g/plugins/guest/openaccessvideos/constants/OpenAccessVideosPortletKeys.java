c7d9e6c0ffe816be8f0f5948e88f81531de30c6a
isturm <iavor.sturm@uni-hamburg.de>
2017-12-18 11:33:02 +0100

    module structure updated.

---
+++ de.uhh.l2g.plugins.guest.openaccessvideos/src/main/java/de/uhh/l2g/plugins/guest/openaccessvideos/constants/OpenAccessVideosPortletKeys.java
@@ -0,0 +1,10 @@
+package de.uhh.l2g.plugins.guest.openaccessvideos.constants;
+
+/**
+ * @author isturm
+ */
+public class OpenAccessVideosPortletKeys {
+
+	public static final String OpenAccessVideos = "OpenAccessVideos";
+
+}
