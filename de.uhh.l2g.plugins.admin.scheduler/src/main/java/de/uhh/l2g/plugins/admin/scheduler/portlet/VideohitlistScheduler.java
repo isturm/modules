b38d1abba6a9016e37c64f974c7d8d1d27e9c791
isturm <iavor.sturm@uni-hamburg.de>
2018-01-12 13:44:23 +0100

    New scheduler added to scheduler module.

---
+++ de.uhh.l2g.plugins.admin.scheduler/src/main/java/de/uhh/l2g/plugins/admin/scheduler/portlet/VideohitlistScheduler.java
@@ -0,0 +1,108 @@
+package de.uhh.l2g.plugins.admin.scheduler.portlet;
+
+/**
+ * The Class DatabaseThread.
+ */
+public final class VideohitlistScheduler {
+//
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = 1L;
+//	private static Log LOG;	
+//	
+//    public VideohitlistScheduler(){
+//    	super();
+//    	LOG = LogFactoryUtil.getLog(VideohitlistScheduler.class.getName());
+//    }
+//    
+//	public VideohitlistScheduler(String schedulerClassName, ServiceContext serviceContext) {
+//		super(VideohitlistScheduler.class.getName(), serviceContext);
+//	    this.schedulerClassName = VideohitlistScheduler.class.getName();
+//	    LOG = LogFactoryUtil.getLog(this.schedulerClassName);
+//	}
+	
+//	@Override
+//    public void receive(Message message) throws MessageListenerException {
+//	   //uncoment for further debug messages
+//	   //super.receive(message);
+//		LOG.info("Videohitlist Scheduler running "+message.getValues().get(SchedulerEngine.JOB_NAME).toString()+"...");
+//	   //Do Job
+//		ThreadManagement tm = new ThreadManagement();
+//		try {
+//			tm.createPopularVideoList();
+//			LOG.info("Videohitlist Scheduler finished.");
+//		} catch (SystemException e) {
+//			LOG.info("Videohitlist Scheduler failed.");
+//		}
+//    }	
+//	
+//	public void start() {
+//        super.schedule();
+//        LOG.info("Videohitlist Scheduler start.");
+//	}
+//	
+//	public void stop() {
+//		super.unschedule();
+//		LOG.info("Videohitlist Scheduler stop.");
+//	}
+//
+//	public void init(String schedulerClassName, ServiceContext serviceContext) {
+//        super.initScheduler(schedulerClassName,serviceContext);
+//	}
+//
+//	private static Long HITS=new Long(20);
+//
+//	public void createPopularVideoList() throws SystemException {
+//		List<Video> returnList = new ArrayList<Video>();
+//		//1.get open access videos with more than 20 clicks 
+//		returnList = VideoLocalServiceUtil.getByHitsAndOpenAccess(HITS);
+//		//2.truncate table videohitlist
+//		VideohitlistLocalServiceUtil.deleteAll();
+//		//3.fill table with data
+//		Calendar calendar = new GregorianCalendar(); 
+//		calendar.setTimeZone( TimeZone.getTimeZone("CET") );
+//		long msnow = calendar.getTimeInMillis();
+//
+//		Date d1 = new Date();
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm");
+//
+//		for(Video v : returnList){
+//			try {
+//				d1 = df.parse(v.getGenerationDate()); 
+//				long ms1   = d1.getTime();
+//				long hits = v.getHits();
+//				long timeinms = msnow - ms1;
+//
+//				// Durschnittswerte berechnen
+//				//Berechne alter des Videos in...
+//				long days = timeinms / (1000*60*60*24); //...Tagen
+//				long week = timeinms / (1000*60*60*24*7); //...Wochen
+//				long month = timeinms / 2628000000l; //....Monaten
+//				long year = timeinms / (2628000000l*12l); //....Jahren
+//				
+//				//Berechne die Hits pro...
+//				long clicksperday = calcHitsPro(days, hits);
+//				long clicksperweek = calcHitsPro(week, hits);
+//				long clickspermonth = calcHitsPro(month, hits);
+//				long clicksperyear = calcHitsPro(year, hits);
+//
+//				Videohitlist vhl = new VideohitlistImpl();
+//				vhl.setVideoId(v.getVideoId());
+//				vhl.setHitsPerDay(clicksperday);
+//				vhl.setHitsPerWeek(clicksperweek);
+//				vhl.setHitsPerMonth(clickspermonth);
+//				vhl.setHitsPerYear(clicksperyear);
+//				//save
+//				VideohitlistLocalServiceUtil.addVideohitlist(vhl);
+//				}catch (ParseException e) {
+//					System.out.println("Simple Date Parsen Error!!");
+//				}
+//		}
+//	}	
+//	
+//	private long calcHitsPro(long einheit, long hits){
+//		if(einheit>=1)return (long) (hits/einheit); //Hits pro Einheit (tag, woche, monat, jahr...)
+//		else return hits; //else: Das Video ist noch kein volles Jahr vollen Monat etc alt.
+//	}
+}
