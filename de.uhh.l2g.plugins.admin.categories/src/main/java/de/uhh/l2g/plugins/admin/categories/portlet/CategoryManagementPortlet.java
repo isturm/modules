84fa1529ef2055ba4843c627898563f71aa2f322
isturm <iavor.sturm@uni-hamburg.de>
2018-01-12 11:30:38 +0100

    module categories updated.

--- de.uhh.l2g.plugins.admin.categories/src/main/java/de/uhh/l2g/plugins/admin/categories/portlet/CategoryManagementPortlet.java
+++ de.uhh.l2g.plugins.admin.categories/src/main/java/de/uhh/l2g/plugins/admin/categories/portlet/CategoryManagementPortlet.java
@@ -1,10 +1,21 @@
 package de.uhh.l2g.plugins.admin.categories.portlet;
 
 import de.uhh.l2g.plugins.admin.categories.constants.CategoryManagementPortletKeys;
+import de.uhh.l2g.plugins.model.Category;
+import de.uhh.l2g.plugins.service.CategoryLocalServiceUtil;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -25,5 +36,50 @@ import org.osgi.service.component.annotations.Component;
 	},
 	service = Portlet.class
 )
+
 public class CategoryManagementPortlet extends MVCPortlet {
+
+	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse) throws PortletException, IOException {
+		//
+	}
+		
+	public void add(ActionRequest request, ActionResponse response) throws SystemException, PortalException{
+		String backURL = request.getParameterMap().get("backURL")[0];
+		String name=request.getParameter("name");
+		Category category = CategoryLocalServiceUtil.createCategory(0);
+		category.setName(name);
+		CategoryLocalServiceUtil.addCategory(category);
+		try {
+			response.sendRedirect(backURL);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public void edit(ActionRequest request, ActionResponse response) throws SystemException, PortalException{
+		long reqCategoryId = new Long(request.getParameterMap().get("categoryId")[0]);
+		String backURL = request.getParameterMap().get("backURL")[0];
+		String name=request.getParameter("name");
+
+		Category category = CategoryLocalServiceUtil.getCategory(reqCategoryId);
+		category.setName(name);
+		CategoryLocalServiceUtil.updateCategory(category);
+		try {
+			response.sendRedirect(backURL);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public void delete(ActionRequest request, ActionResponse response) throws SystemException, PortalException{
+		long reqCategoryId = new Long(request.getParameterMap().get("categoryId")[0]);
+		String backURL = request.getParameterMap().get("backURL")[0];
+		//Video_Term, Lecture_Term, Term
+		CategoryLocalServiceUtil.deleteById(reqCategoryId);
+		try {
+			response.sendRedirect(backURL);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
