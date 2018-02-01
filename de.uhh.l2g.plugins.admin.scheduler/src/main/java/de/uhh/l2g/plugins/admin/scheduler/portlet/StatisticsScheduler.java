9b93deb0234fe7570a9b938d0aca450978e3375f
isturm <iavor.sturm@uni-hamburg.de>
2018-02-01 12:11:18 +0100

    Statistic Scheduler Update.

--- de.uhh.l2g.plugins.admin.scheduler/src/main/java/de/uhh/l2g/plugins/admin/scheduler/portlet/StatisticsScheduler.java
+++ de.uhh.l2g.plugins.admin.scheduler/src/main/java/de/uhh/l2g/plugins/admin/scheduler/portlet/StatisticsScheduler.java
@@ -1,78 +1,188 @@
 package de.uhh.l2g.plugins.admin.scheduler.portlet;
-/***************************************************************************
- * The Lecture2Go software is based on the liferay portal 6.1.1
- * <http://www.liferay.com>
- *
- * Lecture2Go <http://lecture2go.uni-hamburg.de> is an open source
- * platform for media management and distribution. Our goal is to
- * support the free access to knowledge because this is a component
- * of each democratic society. The open source software is aimed at
- * academic institutions and has to strengthen the blended learning.
- *
- * All Lecture2Go plugins are continuously being developed and improved.
- * For more details please visit <http://lecture2go-open-source.rrz.uni-hamburg.de>
- *
- * @Autor Lecture2Go Team
- * @Version 1.0
- * @Contact lecture2go-open-source@uni-hamburg.de
- *
- * Copyright (c) 2013 University of Hamburg / Computer and Data Center (RRZ)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- ***************************************************************************/
+
+import java.util.Date;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.BaseSchedulerEntryMessageListener;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.scheduler.SchedulerEngine;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerException;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.StorageTypeAware;
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import de.uhh.l2g.plugins.service.StatisticLocalServiceUtil;
+import de.uhh.l2g.plugins.service.VideoLocalServiceUtil;
 
 /** Statistics is less flawed when running job at concrete time (ideally around midnight)  
  *  
  * The Class StatisticsSheduler.
  */
-public class StatisticsScheduler  {  
-//	private static Log LOG;	
-//	  
-//    public StatisticsScheduler(){
-//    	super();
-//    	LOG = LogFactoryUtil.getLog(StatisticsScheduler.class.getName());
-//    }
-//    
-//	@Override
-//    public void receive(Message message) throws MessageListenerException {
-//	   //uncoment for further debug messages
-//	   //super.receive(message);
-//	   LOG.info("Statistics Scheduler running "+message.getValues().get(SchedulerEngine.JOB_NAME).toString()+"...");
-//	   //Do Job....
-//	   int privateVideos = 0;
-//	   int publicVideos = 0;
-//	   try {
-//		   publicVideos=VideoLocalServiceUtil.getByOpenAccess(1).size();
-//		   privateVideos=VideoLocalServiceUtil.getByOpenAccess(0).size();
-//		   //TODO can not get the service context for using the addEntry method, because of the scheduler! And can't find workaround.
-//		   StatisticLocalServiceUtil.add(privateVideos, publicVideos);
-//	   } catch (PortalException e) {
-//		   LOG.info("Statistics Scheduler failed.");
-//	   } catch (SystemException e) {
-//		   LOG.info("Statistics Scheduler failed.");
-//	   }
-//	   //Job end
-//	   LOG.info("Statistics Scheduler finished.");
-//    }
-//	
-//	public void start() {
-//        super.schedule();
-//	}
-//	
-//	public void stop() {
-//		super.unschedule();
-//	}
+@Component(
+		  immediate = true, 
+		  property = {"cron.expression=0/60 * * * * ?"},
+		  service = StatisticsScheduler.class
+)
+public class StatisticsScheduler  extends BaseSchedulerEntryMessageListener {  
+
+	  /**
+	   * doReceive: This is where the magic happens, this is where you want to do the work for
+	   * the scheduled job.
+	   * @param message This is the message object tied to the job.  If you stored data with the
+	   *                job, the message will contain that data.   
+	   * @throws Exception In case there is some sort of error processing the task.
+	   */
+	  @Override
+	  protected void doReceive(Message message) throws Exception {
+		   //uncoment for further debug messages
+		   //super.receive(message);
+		  _log.info("Statistics Scheduler running "+message.getValues().get(SchedulerEngine.JOB_NAME).toString()+"...");
+		   //Do Job....
+		   int privateVideos = 0;
+		   int publicVideos = 0;
+		   try {
+			   publicVideos=VideoLocalServiceUtil.getByOpenAccess(1).size();
+			   privateVideos=VideoLocalServiceUtil.getByOpenAccess(0).size();
+			   //TODO can not get the service context for using the addEntry method, because of the scheduler! And can't find workaround.
+			   StatisticLocalServiceUtil.add(privateVideos, publicVideos);
+		   } catch (PortalException e) {
+			   _log.info("Statistics Scheduler failed.");
+		   } catch (SystemException e) {
+			   _log.info("Statistics Scheduler failed.");
+		   }
+		   //Job end
+		   _log.info("Statistics Scheduler finished.");
+	  }
+
+	  
+	  /**
+	   * activate: Called whenever the properties for the component change (ala Config Admin)
+	   * or OSGi is activating the component.
+	   * @param properties The properties map from Config Admin.
+	   * @throws SchedulerException in case of error.
+	   */
+	  @Activate
+	  @Modified
+	  protected void activate(Map<String,Object> properties) throws SchedulerException {
+
+	    // extract the cron expression from the properties
+	    String cronExpression = GetterUtil.getString(properties.get("cron.expression"), _DEFAULT_CRON_EXPRESSION);
+
+	    // create a new trigger definition for the job.
+	    String listenerClass = getClass().getName();
+	    Trigger jobTrigger = _triggerFactory.createTrigger(listenerClass, listenerClass, new Date(), null, cronExpression);
+
+	    // wrap the current scheduler entry in our new wrapper.
+	    // use the persisted storaget type and set the wrapper back to the class field.
+	    _schedulerEntryImpl = new SchedulerEntryImpl();
+	    _schedulerEntryImpl.setEventListenerClass(listenerClass);
+	    _schedulerEntryImpl.setTrigger(jobTrigger);
+	    _schedulerEntryImpl = new StorageTypeAwareSchedulerEntryImpl(_schedulerEntryImpl);
+
+	    // update the trigger for the scheduled job.
+	    _schedulerEntryImpl.setTrigger(jobTrigger);
+
+	    // if we were initialized (i.e. if this is called due to CA modification)
+	    if (_initialized) {
+	      // first deactivate the current job before we schedule.
+	      deactivate();
+	    }
+
+	    // register the scheduled task
+	    _schedulerEngineHelper.register(this, _schedulerEntryImpl, DestinationNames.SCHEDULER_DISPATCH);
+
+	    // set the initialized flag.
+	    _initialized = true;
+	  }
+
+	  /**
+	   * deactivate: Called when OSGi is deactivating the component.
+	   */
+	  @Deactivate
+	  protected void deactivate() {
+	    // if we previously were initialized
+	    if (_initialized) {
+	      // unschedule the job so it is cleaned up
+	      try {
+	        _schedulerEngineHelper.unschedule(_schedulerEntryImpl, getStorageType());
+	      } catch (SchedulerException se) {
+	        if (_log.isWarnEnabled()) {
+	          _log.warn("Unable to unschedule trigger", se);
+	        }
+	      }
+
+	      // unregister this listener
+	      _schedulerEngineHelper.unregister(this);
+	    }
+	    
+	    // clear the initialized flag
+	    _initialized = false;
+	  }
+
+	  /**
+	   * getStorageType: Utility method to get the storage type from the scheduler entry wrapper.
+	   * @return StorageType The storage type to use.
+	   */
+	  protected StorageType getStorageType() {
+	    if (_schedulerEntryImpl instanceof StorageTypeAware) {
+	      return ((StorageTypeAware) _schedulerEntryImpl).getStorageType();
+	    }
+	    
+	    return StorageType.MEMORY_CLUSTERED;
+	  }
+	  
+	  /**
+	   * setModuleServiceLifecycle: So this requires some explanation...
+	   * 
+	   * OSGi will start a component once all of it's dependencies are satisfied.  However, there
+	   * are times where you want to hold off until the portal is completely ready to go.
+	   * 
+	   * This reference declaration is waiting for the ModuleServiceLifecycle's PORTAL_INITIALIZED
+	   * component which will not be available until, surprise surprise, the portal has finished
+	   * initializing.
+	   * 
+	   * With this reference, this component activation waits until portal initialization has completed.
+	   * @param moduleServiceLifecycle
+	   */
+	  @Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	  protected void setModuleServiceLifecycle(ModuleServiceLifecycle moduleServiceLifecycle) {
+	  }
+
+	  @Reference
+	  protected void setTriggerFactory(TriggerFactory triggerFactory) {
+	    _triggerFactory = triggerFactory;
+	  }
+
+	  @Reference
+	  protected void setSchedulerEngineHelper(SchedulerEngineHelper schedulerEngineHelper) {
+	    _schedulerEngineHelper = schedulerEngineHelper;
+	  }
+
+	  // the default cron expression is to run daily at midnight
+	  private static final String _DEFAULT_CRON_EXPRESSION = "0 0 0 * * ?";
+
+	  private static final Log _log = LogFactoryUtil.getLog(StatisticsScheduler.class);
+
+	  private volatile boolean _initialized;
+	  private TriggerFactory _triggerFactory;
+	  private SchedulerEngineHelper _schedulerEngineHelper;
+	  private SchedulerEntryImpl _schedulerEntryImpl = null;
     
 }
  
